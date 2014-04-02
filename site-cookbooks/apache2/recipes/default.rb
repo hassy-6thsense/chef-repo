7605951ab32a273a0fb7f88c9ec470fe7537a5fd
ohashia <m133302s@st.u-gakugei.ac.jp>
2014-04-02 17:55:59 +0900

    Apache2のインストール

--- site-cookbooks/apache2/recipes/default.rb
+++ site-cookbooks/apache2/recipes/default.rb
@@ -6,3 +6,37 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+
+package "apache2" do
+	case node[ :platform ]
+	when "debian", "ubuntu"
+		package_name "apache2"
+	when "redhat", "centos", "fedora"
+		package_name "httpd"
+	end
+end
+
+
+template "index.html" do
+	case node[ :platform ]
+	when "debian", "ubuntu"
+		path "/var/www/index.html"
+	when "redhat", "centos", "fedora"
+		path "/var/www/html/index.html"
+	end
+
+	source "index.html.erb"
+	mode 0644
+	variables( { :fqdn => node[ :fqdn ] } )
+end
+
+
+service "apache2" do
+	case node[ :platform ]
+	when "debian", "ubuntu"
+		service_name "apache2"
+	when "redhat", "centos", "fedora"
+		service_name "httpd"
+	end
+	action [ :enable, :start ]
+end
